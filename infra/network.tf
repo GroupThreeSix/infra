93f11093316853ec474b58f988db9f40fae52e28
tuilakhanh <bacdau852@gmail.com>
2024-12-18 11:36:59 +0700

    feat: Add network security group

--- infra/network.tf
+++ infra/network.tf
@@ -18,33 +18,15 @@ resource "azurerm_subnet" "aks_subnet" {
   depends_on = [azurerm_virtual_network.vnet]
 }
 
-# resource "azurerm_network_security_group" "aks_nsg" {
-#   name                = var.cluster_network_security_name
-#   location            = azurerm_resource_group.cluster_manager.location
-#   resource_group_name = azurerm_resource_group.cluster_manager.name
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "${var.k8s_name}-nsg"
+  location            = azurerm_resource_group.k8s.location
+  resource_group_name = azurerm_resource_group.k8s.name
+}
 
-#   dynamic "security_rule" {
-#     for_each = var.cluster_network_security_rules
+resource "azurerm_subnet_network_security_group_association" "aks_subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.aks_subnet.id
+  network_security_group_id = azurerm_network_security_group.aks_nsg.id
 
-#     content {
-#       name                       = security_rule.value.sg_name
-#       priority                   = security_rule.value.sg_priority
-#       direction                  = security_rule.value.sg_direction
-#       access                     = security_rule.value.sg_access
-#       protocol                   = security_rule.value.sg_protocol
-#       source_port_range          = security_rule.value.sg_source_port_range
-#       destination_port_range     = security_rule.value.sg_destination_port_range
-#       source_address_prefix      = security_rule.value.sg_source_address_prefix
-#       destination_address_prefix = security_rule.value.sg_destination_address_prefix
-#     }
-#   }
-
-#   depends_on = [azurerm_resource_group.cluster_manager]
-# }
-
-# resource "azurerm_subnet_network_security_group_association" "aks_subnet_nsg_association" {
-#   subnet_id                 = azurerm_subnet.aks_subnet.id
-#   network_security_group_id = azurerm_network_security_group.aks_nsg.id
-
-#   depends_on = [azurerm_network_security_group.aks_nsg, azurerm_subnet.aks_subnet]
-# }
+  depends_on = [azurerm_network_security_group.aks_nsg, azurerm_subnet.aks_subnet]
+}
